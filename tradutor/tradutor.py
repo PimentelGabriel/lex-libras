9ec4cffc27ba960ee5b6b7e6f4695f6ad9964e96
PimentelGabriel <gabrielcosta.tads@gmail.com>
2022-10-10 19:35:50 -0300

    Arquivos preparados para receber funcionalidade modularizada

--- tradutor/tradutor.py
+++ tradutor/tradutor.py
@@ -1,1 +1,12 @@
-from tradutor.functions.graph_morph_changer import graphMorphChanger
+import spacy
+
+from functions.core_translater import coreTranslater
+from functions.graph_morph_changer import graphMorphChanger
+from functions.sintatic_organizer import sintaticOrganizer
+
+
+nlp = spacy.load("pt_core_news_lg")
+doc = nlp("Vamos combater o mosquito da dengue!")
+
+for token in doc:
+    print(token.text, token.lemma_.upper(), token.pos_, token.dep_)
