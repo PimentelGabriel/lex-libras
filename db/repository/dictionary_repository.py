4bc09f04a4759978b37d918f6a3ac0b8d2d35513
Gabriel Pimentel <gabrielcosta.tads@gmail.com>
2022-10-14 18:01:10 -0300

    Implementando a função de resgate de palavras lematizadas de um texto

--- db/repository/dictionary_repository.py
+++ db/repository/dictionary_repository.py
@@ -16,3 +16,16 @@ class DictionaryRepository:
                     Dictionary.path_video
                 )\
                 .all()
+
+    def selectPalavras(self, lemmas):
+        palavrasCandidatas = ''
+        # Fazer uam raw query no SQLAlchemy usando o statment 'where palavra in (lemas[0], lemas[1], lemas[2], lemas[3])'
+        for palavra in lemmas:
+            if lemas.index(palavra) == 0:
+                palavrasCandidatas = f'\'{palavra}\''
+            else:       
+                palavrasCandidatas += f', \'{palavra}\''
+
+        F'SELECT distinct(palavra) FROM palavras WHERE palavra IN ({palavrasCandidatas})'
+
+        SELECT distinct(palavra) FROM palavras WHERE palavra IN ('IR', 'PARA', 'O', 'PRAIA')
