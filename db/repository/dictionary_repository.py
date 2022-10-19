e99f3ce10665b22df3912252354231ce8c3bae0e
Gabriel Pimentel <gabrielcosta.tads@gmail.com>
2022-10-19 12:18:13 -0300

    Pre preparo de raw query sql para buscar palavras no banco

--- db/repository/dictionary_repository.py
+++ db/repository/dictionary_repository.py
@@ -18,21 +18,43 @@ class DictionaryRepository:
                 .all()
 
     def selectPalavras(self, lemmas):
-        palavrasCandidatas = ''
+        palavrasCandidatas = ""
         # Fazer uam raw query no SQLAlchemy usando o statment 'where palavra in (lemas[0], lemas[1], lemas[2], lemas[3])'
         for palavra in lemmas:
-            if lemas.index(palavra) == 0:
+            if lemmas.index(palavra) == 0:
                 palavrasCandidatas = f'\'{palavra}\''
             else:       
                 palavrasCandidatas += f', \'{palavra}\''
 
         # print(f'SELECT distinct(palavra) FROM palavras WHERE palavra IN ({palavrasCandidatas})');
+        print("palavrasCandidatas")
         print(palavrasCandidatas)
 
+        query = f'SELECT distinct(p.palavra) as palavra, c.nome, c.flag FROM palavras AS p, classes_gramaticais AS c WHERE p.palavra IN ({palavrasCandidatas}) AND p.classe_gramatical like c.id GROUP BY p.palavra;'
+
+        result = None
         with DBConnectionHandler() as conn:
-            resp = conn.runSQLRaw("SELECT p.id, p.palavra, c.nome FROM palavras AS p, classes_gramaticais AS c IN (:lemmas);", ({"lemmas": palavrasCandidatas}))
-            self.lastResult = resp
-            for row in resp:
-                print(row)
+            # resp = conn.runSQLRaw("SELECT p.id, p.palavra, c.nome FROM palavras AS p, classes_gramaticais AS c IN ( :lemmas );", {"lemmas": palavrasCandidatas})
+            resp = None
+            try:
+                resp = conn.runSQLRaw(query)
+                self.lastResult = resp
+                print(query)
+
+                for r in resp:
+                    print(r)
+
+    
+            except Exception as e:
+                print(e)
+            # print(type(resp))
+            # print(len(resp))
+
+            return resp
+
+        return []
+
+            # for row in resp:
+            #     print(row)
 
         # SELECT distinct(palavra) FROM palavras WHERE palavra IN ('IR', 'PARA', 'O', 'PRAIA')
