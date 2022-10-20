ac103f2fac343eb2814ec02015400eb28585561d
Gabriel Pimentel <gabrielcosta.tads@gmail.com>
2022-10-20 19:54:20 -0300

    Agora é removido todas as adposições (no caso preposições)

--- lex_libras/functions/core_translater/functions/removeAdp.py
+++ lex_libras/functions/core_translater/functions/removeAdp.py
@@ -1,9 +1,10 @@
-# Essa função deve remover as adposições* de uma frase PT-br para não ser usada em uma frase LIBRAS
+# Essa função deve remover as adposições* (preposições e pósposições)
+# de uma frase PT-br para não ser usada em uma frase LIBRAS
 # https://universaldependencies.org/u/pos/ADP.html
 
 def removeAdp(token):
     if  token.pos_ == "ADP":
-        if token.text.upper() in ("A", "O", "AS", "OS"):
-            token._.eh_corresponde = False
+        # if token.text.upper() in ("A", "O", "AS", "OS", "DA", "DO", "DAS", "DOS"):
+        token._.eh_corresponde = False
 
     # print("removeArt in implementing state")
