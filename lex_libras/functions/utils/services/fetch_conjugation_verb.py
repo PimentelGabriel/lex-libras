0b430eee5a90a337dc6fb657bc231e6a368318b7
Gabriel Pimentel <gabrielcosta.tads@gmail.com>
2022-11-21 13:54:02 -0300

    Organização do arquivos de frases e add nova função fetch_conjugation_verb
    O arquivo fetch_conjugation_verb obtem, atravês de web scraping, os dados de verbo, obtento o lema (lemma_) do verbo.
    Alguns verbos no passado, ao acessar o atributo TOKEN.lemma_ do spacy, retornava-se o verbo no passado, porém o banco só possui verbos no presente, impossibilitanto assim a busca no banco de dados.

---
+++ lex_libras/functions/utils/services/fetch_conjugation_verb.py
@@ -0,0 +1,54 @@
+import os, requests as req
+from bs4 import BeautifulSoup as bs
+
+def fetchConjugation(data):
+    HEADERS = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:106.0) Gecko/20100101 Firefox/106.0"
+    }
+
+    # url = "https://www.dicionarioinformal.com.br/flexoes/falares/"
+    URL = "https://www.dicionarioinformal.com.br/flexoes/"
+    # div[class='col-xs-12'] > h1 > a
+    
+    if type(data) == 'str':
+        # if os.environ['LEXLIBRAS_VERBOSE'] == "1":
+        page = req.get(
+            URL+data,
+            HEADERS
+        )
+
+        body = bs(page.content, 'html.parser')
+
+        palavra = body.select("cssQuery:string")
+
+        return None
+    else:
+        raise Exception(f"Param's receive not string not implemented yet")
+
+
+def fetchInfinitiveForm(data):
+    HEADERS = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:106.0) Gecko/20100101 Firefox/106.0"
+    }
+
+    # url = "https://www.dicionarioinformal.com.br/flexoes/falares/"
+    URL = "https://www.dicionarioinformal.com.br/flexoes/"
+    # div[class='col-xs-12'] > h1 > a
+    
+    print(f"\n\n\t\tHERE1\n{type(data)}\n\n")
+    if isinstance(data, str):
+        print("\n\n\t\tHERE2\n\n")
+        # if os.environ['LEXLIBRAS_VERBOSE'] == "1":
+        page = req.get(
+            URL+data,
+            HEADERS
+        )
+
+        body = bs(page.content, 'html.parser')
+
+        palavra = body.select("div[class='col-xs-12'] > h1 > a")
+
+        return palavra[0].contents[0].upper()
+
+    else:
+        raise Exception(f"Param's receive not string not implemented yet")
