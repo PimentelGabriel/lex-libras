5fecadafb7ccb7952fb706853a6ad18be72da14c
Gabriel Pimentel <gabrielcosta.tads@gmail.com>
2022-11-30 14:52:16 -0300

    Add modo verboso para log

--- db/configs/connection.py
+++ db/configs/connection.py
@@ -1,6 +1,8 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.sql import text
+
 
 class DBConnectionHandler():
     def __init__(self):
@@ -10,9 +12,15 @@ class DBConnectionHandler():
         self.session = session_maker()
 
     def __create_database_engine(self):
+        engineEcho = True
+        if os.environ['LEXLIBRAS_VERBOSE'] == "1":
+            engineEcho = True
+        else:
+            engineEcho = False
+
         return create_engine(
             self.__connection_string,
-            echo=True,
+            echo=engineEcho,  # Acesso de dados em mobo verboso
             future=False  # Verificar pq quando True, dar erro
         )
 
@@ -20,24 +28,34 @@ class DBConnectionHandler():
         return self.__engine
 
     def runSQLRaw(self, query, data=[{"index": "index"}]):
-        eng = self.__get_engine()
-        conn = eng.connect()
-        
-        # return conn.execute("SELECT palavra FROM palavras LIMIT 3;")
-        
-        statement = text(query)
+        try:
+            eng = self.__get_engine()
+            conn = eng.connect()
 
-        if data[0]["index"] == "index":
-            # print("1º execute")
-            return conn.execute(query)
-        else:
-            # print("2º execute")
-            result = []
-            for line in data:
-                # print("Exeted")
-                result.append(conn.execute(statement, **line))
-                
-            return result
+            # return conn.execute("SELECT palavra FROM palavras LIMIT 3;")
+
+            statement = text(query)
+
+            print(F"\n\n\tLOG DO runSQLRaw\n\t{__file__ }\n\t {__doc__}")
+            print(f"statement: {statement}")
+            print(f"Tipo: {type(statement)}")
+            print(f"query: {query}")
+            print(f"Tipo: {type(query)}\n\n")
+
+            if data[0]["index"] == "index":
+                print("1º execute")
+
+                return conn.execute(query)
+            else:
+                print("2º execute")
+                result = []
+                for line in data:
+                    # print("Exeted")
+                    result.append(conn.execute(statement, **line))
+
+                return result
+        except Exception as e:
+            print(e)
 
     def __enter__(self):
         session_maker = sessionmaker(bind=self.__engine)
