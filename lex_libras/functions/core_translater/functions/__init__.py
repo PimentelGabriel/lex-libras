94b48a2dc57dd4f3dbac1a9b215107f445fa59b5
Gabriel Pimentel <gabrielcosta.tads@gmail.com>
2022-12-08 14:52:16 -0300

    Add encontrarAlias
    Essa função analisa formas de palavras numa frase que, quando pedido ao token.lemma_ (do spacy) não retorna a forma da palavra presente no banco de dados, por isso essa função faz um web scraping para obter a forma infinitiva ou aceitável no banco de dados

--- lex_libras/functions/core_translater/functions/__init__.py
+++ lex_libras/functions/core_translater/functions/__init__.py
@@ -2,3 +2,4 @@ from .removeArt import removeArt
 from .removeConj import removeConj
 from .removeAdp import removeAdp
 from .aglutinarPalavra import aglutinarPalavra
+from .encontrarAlias import encontrarAlias
--- lex_libras/functions/core_translater/functions/encontrarAlias.py
+++ lex_libras/functions/core_translater/functions/encontrarAlias.py
@@ -1,2 +1,55 @@
-def encontrarAlias(Doc):
+from lex_libras.dto.palavraCandidataDTO import palavraCandidataDTO
+from lex_libras.functions.utils.services.fetch_conjugation_verb import fetchInfinitiveForm
+
+
+def encontrarAlias(Doc) -> None:
     for token in Doc:
+        print(
+            f"\t\t[NOTICE]: Fething: {token.text}\n\t\t\ttoken._.eh_corresponde: {token._.eh_corresponde}")
+        if token._.eh_corresponde:
+            if token.pos_ == 'VERB':
+                print(token.pos_)
+                checked = False
+                # Procura o a caracteristica especifica para ver se a palavra é selecionável
+
+                # Ver se a palavra é um pronome
+                for c in token.children:
+                    if c.pos_ == "PRON" and c.dep_ == "nsubj":
+                        print("In for")
+                        checked = True
+                        break
+                print("Next out for")
+
+                if checked:
+                    checked = False
+                    if token.morph.get("VerbForm")[0] == "Fin" and token.morph.get("Mood")[0] == "Sub":
+                        print(f"\t\t[NOTICE]: Fething: {token.text}")
+                        palavra = fetchInfinitiveForm(token.text)
+
+                        Doc._.candidateWords.addNewCandidateWord(
+                            palavraCandidataDTO(
+                                token.i,
+                                palavra,
+                                2
+                            )
+                        )
+
+                    return None
+
+                print(token.morph)
+
+                if token.morph.get('Tense'):
+                    if token.morph.get('Tense')[0] == "Past":
+                        print(token.morph.get('Tense'))
+                        palavra = fetchInfinitiveForm(token.text)
+                        Doc._.candidateWords.addNewCandidateWord(
+                            palavraCandidataDTO(
+                                token.i,
+                                palavra,
+                                2
+                            )
+                        )
+
+                    return None
+
+                return None
